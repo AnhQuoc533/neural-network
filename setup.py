693428b035d01d1558570598ef5755be1ed77aa8
Quoc <duyenvothi3@gmail.com>
2023-02-25 16:43:59 -0500

    Add pyproject.toml

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@
 
 from setuptools import setup
 import pathlib
-import neural_network
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
@@ -11,25 +10,33 @@ HERE = pathlib.Path(__file__).parent
 # The text of the README file
 README = (HERE / "README.md").read_text()
 
+# Determine version number from module
+VERSION = None
+with (HERE / "neural_network" / "__init__.py").open() as f:
+    for line in f:
+        if line.startswith("__version__ ="):
+            VERSION = line.strip().split()[2][1:-1]
+            break
+
 setup(name='neural-network',
-      version=neural_network.__version__,
+      version=VERSION,
       description='A Python package for building multi-layer perceptron (a basic neural network).',
       long_description=README,
       long_description_content_type="text/markdown",
       url="https://github.com/AnhQuoc533/neural-network",
-      packages=['neural_network'],
+      packages=['neural_network'],  # packages=find_packages(exclude=("test",))
       author='Anh Quoc',
       author_email='lhoanganhquoc@gmail.com',
       license='MIT',
       classifiers=[
-            'Development Status :: 3 - Alpha',
-            'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.8',
-            'Programming Language :: Python :: 3.9',
-            'Programming Language :: Python :: 3.10'
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10'
       ],
-      keywords='neural-network, AI, deep-learning, ai, machine-learning, neural-networks',
+      keywords='neural-network, deep-learning, machine-learning, neural-networks, machine-learning-algorithms',
       install_requires=["numpy>=1.22.1", "matplotlib>=3.5.1"],
       python_requires='>=3.8',
       zip_safe=False,

1268f8bd55533705d0359f6ae7fc7dbf70956cfc
Quoc <duyenvothi3@gmail.com>
2023-02-25 14:45:31 -0500

    Edit setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
-# https://packaging.python.org/en/latest/tutorials/packaging-projects/
+# https://packaging.python.org/en/latest/tutorials/packaging-projects
+# https://docs.python.org/3/tutorial/modules.html#packages
+
 from setuptools import setup
 import pathlib
-import neural_networks
+import neural_network
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
@@ -9,32 +11,31 @@ HERE = pathlib.Path(__file__).parent
 # The text of the README file
 README = (HERE / "README.md").read_text()
 
-setup(name='neural-networks',
-      version=neural_networks.__version__,
-      description='Basic neural networks package.',
+setup(name='neural-network',
+      version=neural_network.__version__,
+      description='A Python package for building multi-layer perceptron (a basic neural network).',
       long_description=README,
       long_description_content_type="text/markdown",
-      url="https://github.com/AnhQuoc533/neural-networks",
-      packages=['neural_networks'],
+      url="https://github.com/AnhQuoc533/neural-network",
+      packages=['neural_network'],
       author='Anh Quoc',
       author_email='lhoanganhquoc@gmail.com',
       license='MIT',
       classifiers=[
-            'Development Status :: 1 - Planning',
+            'Development Status :: 3 - Alpha',
             'License :: OSI Approved :: MIT License',
             'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9',
             'Programming Language :: Python :: 3.10'
       ],
-      keywords='neural-network AI deep-learning ai',
-      install_requires=["numpy", "matplotlib"],
-      python_requires='>=3.7',
+      keywords='neural-network, AI, deep-learning, ai, machine-learning, neural-networks',
+      install_requires=["numpy>=1.22.1", "matplotlib>=3.5.1"],
+      python_requires='>=3.8',
       zip_safe=False,
       project_urls={  # Optional
-          "Bug Reports": "https://github.com/AnhQuoc533/neural-networks/issues",
+          "Bug Reports": "https://github.com/AnhQuoc533/neural-network/issues",
           "Funding": "https://donate.pypi.org",
-          "Source": "https://github.com/AnhQuoc533/neural-networks",
+          "Source": "https://github.com/AnhQuoc533/neural-network",
       },
       )

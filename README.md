424b6f8e91d355154fa8cd3652445b7f94159e68
Quoc <duyenvothi3@gmail.com>
2023-07-03 21:42:10 -0400

    Add test for momentum and rmsprop

---
+++ README.md
@@ -0,0 +1,121 @@
+<h1 align="center">neural-network</h1>
+
+---
+**neural-network** is a Python package on TestPyPi that provides a Multi-Layer Perceptron (MLP) framework built using only **NumPy**. The framework supports Gradient Descent, Momentum, RMSprop, Adam optimizers.
+
+## Installation
+
+### Dependencies
+```
+python>= 3.8
+numpy>= 1.22.1
+matplotlib>= 3.5.1 
+```
+
+### User installation
+You can install neural-network using `pip`:
+```
+pip install -i https://test.pypi.org/simple/neural-network
+```
+
+## Simple Usage
+
+### Defining the Model Architecture
+To define your model, you need to specify the number of layers, and the number of neurons.
+```python
+from neural_network import NeuralNetwork
+
+model = NeuralNetwork(neurons=[64, 120, 1])
+```
+In this example, we have a four-layer neural network with auto-defined input neurons, 
+first hidden layer with 64 neurons, second hidden layer with 120 neurons, and one output neuron.
+
+### Training the Model
+To train the model, you need to provide input data and the corresponding target data.
+```python
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y = np.array([[0], [1], [1], [0]])
+
+model.fit(X, y, epochs=1000, learning_rate=0.1, optimizer='adam')
+```
+When training the model without manually setting the parameters, the activation functions, or/and the loss functions,
+the framework will automatically do the job for you.
+It will initialize the parameters, and the functions according to the type of model (regression or classification) and
+its architecture.
+
+### Making Predictions
+Once the model is trained, you can use it to make predictions.
+```python
+predictions = model.predict(X)
+```
+
+## Beyond the Framework
+Apart from the neural network framework, the package also provides:
+### Activation functions
+<table>
+<tr>
+    <td><a href="https://en.wikipedia.org/wiki/Sigmoid_function">Sigmoid function</a></td>
+    <td><code>sigmoid()</code></td>
+</tr>
+<tr>
+    <td><a href="https://www.medcalc.org/manual/tanh-function.php">Hyperbolic tangent function</a></td>
+    <td><code>tanh()</code></td>
+</tr>
+<tr>
+    <td><a href="https://paperswithcode.com/method/relu">Rectified linear unit</a></td>
+    <td><code>relu()</code></td>
+</tr>
+<tr>
+    <td><a href="https://paperswithcode.com/method/leaky-relu">Leaky Rectified linear unit</a></td>
+    <td><code>leaky_relu()</code></td>
+</tr>
+<tr>
+    <td><a href="https://en.wikipedia.org/wiki/Softmax_function">Softmax function</a></td>
+    <td><code>softmax()</code></td>
+</tr>
+<tr>
+    <td><a href="https://paperswithcode.com/method/gelu">Gaussian error linear unit</a></td>
+    <td><code>gelu()</code></td>
+</tr>
+</table>
+
+All above functions have 2 parameters:
+* `x`: The input values. Even though some functions can accept numeric primitive data type,
+  it is advised to use NumPy array.
+* `derivative`: A boolean value indicating whether the function computes the derivative on input `x`. Default is False.
+
+### Loss functions
+<table>
+<tr>
+    <td><a href="">Logistic loss function</a></td>
+    <td><code>log_loss()</code></td>
+</tr>
+<tr>
+    <td><a href="">Cross-entropy loss function</a></td>
+    <td><code>cross_entropy_loss()</code></td>
+</tr>
+<tr>
+    <td><a href="">Quadratic loss function</a></td>
+    <td><code>quadratic_loss()</code></td>
+</tr>
+</table>
+
+All above functions have 3 parameters:
+* `y_pred`: Predicted labels. It must be a 2D NumPy array and have the same size as `y_true`.
+* `y_true`: True labels. It must be a 2D NumPy array and have the same size as `y_pred`.
+* `derivative`: A boolean value indicating whether the function computes the derivative. Default is False.
+
+### 2D Decision Boundary Plotting
+This utility function is used for illustrative purpose. It takes a trained binary classification model, 
+a 2D NumPy input data with 2 attributes, and the corresponding binary label data as input. \
+The input model is not necessarily an instance of **NeuralNetwork**, but it must have `predict`
+method that accepts a 2D NumPy array as input.
+```python
+plot_decision_boundary(model, train_x, train_y)
+```
+<p align="center">
+  <img src="img/Figure_1.png">
+</p>
+
+## License
+This project is licensed under the MIT License, as found in the [LICENSE](LICENSE) file.
---
+++ neural_network/test/test_nn.py
@@ -0,0 +1,448 @@
+import pytest
+import h5py
+from sklearn.datasets import make_circles, make_moons
+import matplotlib.pyplot as plt
+from ..utility import plot_decision_boundary
+from ..neural_network import NeuralNetwork
+from ..activation import *
+
+# Load 2D dataset
+data = np.load('datasets/data.npz', allow_pickle=True)
+
+# Load cat images dataset
+with h5py.File('datasets/train_catvnoncat.h5', "r") as train_dataset:
+    train_set_x = np.array(train_dataset["train_set_x"])
+    train_set_y = np.array(train_dataset["train_set_y"]).T
+
+with h5py.File('datasets/test_catvnoncat.h5', "r") as test_dataset:
+    test_set_x = np.array(test_dataset["test_set_x"])
+    test_set_y = np.array(test_dataset["test_set_y"])
+    classes = np.array(test_dataset["list_classes"]).T
+
+# Flatten
+train_set_x = train_set_x.reshape(train_set_x.shape[0], -1)
+test_set_x = test_set_x.reshape(test_set_x.shape[0], -1)
+
+# Normalize
+train_set_x = train_set_x / 255
+test_set_x = test_set_x / 255
+
+# Data for optimizer tests
+moons_X, moons_y = make_moons(n_samples=300, noise=.2, random_state=3)
+
+
+def load_planar_dataset():
+    np.random.seed(1)
+    m = 400  # number of examples
+    N = int(m / 2)  # number of points per class
+    D = 2  # dimensionality
+    X = np.zeros((m, D))  # data matrix where each row is a single example
+    Y = np.zeros(m, dtype=np.uint8)  # labels vector (0 for red, 1 for blue)
+    a = 4  # maximum ray of the flower
+
+    for j in range(2):
+        ix = range(N * j, N * (j + 1))
+        t = np.linspace(j * 3.12, (j + 1) * 3.12, N) + np.random.randn(N) * 0.2  # theta
+        r = a * np.sin(4 * t) + np.random.randn(N) * 0.2  # radius
+        X[ix] = np.c_[r * np.sin(t), r * np.cos(t)]
+        Y[ix] = j
+
+    return X, Y
+
+
+def load_2d_dataset():
+    return data['X'], data['y'], data['Xval'], data['yval']
+
+
+def test_logistic_regression():
+    model = NeuralNetwork(neurons=[1])
+    model.set_params(train_set_x.shape[1], weight={'W1': np.zeros((train_set_x.shape[1], 1))})
+
+    model.fit(train_set_x, train_set_y, learning_rate=0.005, epochs=2000, step=100)
+
+    # Check the costs
+    assert pytest.approx(model.costs[0]) == 0.6931471805599453
+    assert pytest.approx(model.costs[1]) == 0.5845083636993085
+    assert pytest.approx(model.costs[2]) == 0.4669490409465547
+    assert pytest.approx(model.costs[3]) == 0.3760068669480207
+    assert pytest.approx(model.costs[4]) == 0.3314632893282512
+
+    assert pytest.approx(model.costs[-1]) == 0.14087207570310165
+
+    # Check accuracy score
+    assert model.accuracy_score(train_set_x, train_set_y) == 0.9904306220095693
+    assert model.accuracy_score(test_set_x, test_set_y) == 0.70
+
+
+def test_shallow_nn_1():
+    X, Y = load_planar_dataset()
+    np.random.seed(2)
+    neurons = 4
+
+    model = NeuralNetwork([neurons, 1])
+
+    weight = {'W1': np.random.randn(neurons, X.shape[1]).T * 0.01,
+              'W2': np.random.randn(neurons, 1) * 0.01}
+    model.set_params(X.shape[1], weight=weight)
+    model.set_activations([tanh, sigmoid])
+
+    model.fit(X, Y, learning_rate=1.2, epochs=10_000, step=1_000)
+
+    # Check the costs
+    assert pytest.approx(model.costs[0]) == 0.6930480201239823
+    assert pytest.approx(model.costs[1]) == 0.28808329356901846
+    assert pytest.approx(model.costs[2]) == 0.25438549407324607
+
+    assert pytest.approx(model.costs[-2]) == 0.21941059586119502
+    assert pytest.approx(model.costs[-1]) == 0.2185455332506888
+
+    # Check accuracy score
+    assert model.accuracy_score(X, Y) == 0.9075
+
+
+def test_shallow_nn_2():
+    neurons = 7
+    model = NeuralNetwork([neurons, 1])
+    np.random.seed(1)
+    weight = {'W1': np.random.randn(neurons, train_set_x.shape[1]).T * 0.01,
+              'W2': np.random.randn(neurons, 1) * 0.01}
+    model.set_params(train_set_x.shape[1], weight=weight)
+
+    model.fit(train_set_x, train_set_y, learning_rate=0.0075, epochs=2500, step=100)
+
+    # Check the costs
+    assert model.costs[0] == 0.6930497356599891
+    assert model.costs[1] == 0.6464320953428849
+    assert model.costs[2] == 0.6325140647912678
+
+    assert pytest.approx(model.costs[-3]) == 0.05919329501038164
+    assert pytest.approx(model.costs[-2]) == 0.05336140348560552
+    assert pytest.approx(model.costs[-1]) == 0.04855478562877014
+
+    # Check accuracy score
+    assert model.accuracy_score(train_set_x, train_set_y) == 1
+    assert model.accuracy_score(test_set_x, test_set_y) == 0.72
+
+
+def initialize_parameters(layer_dims, seeds=1):
+    np.random.seed(seeds)
+    parameters = {}
+    L = len(layer_dims)  # number of layers in the network
+
+    for l in range(1, L):
+        parameters['W'+str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1]).T / np.sqrt(layer_dims[l-1])
+
+    return parameters
+
+
+def test_deep_nn_1():
+    model = NeuralNetwork([20, 7, 5, 1])
+    weight = initialize_parameters([train_set_x.shape[1], 20, 7, 5, 1])
+    model.set_params(train_set_x.shape[1], weight=weight)
+
+    model.fit(train_set_x, train_set_y, learning_rate=0.0075, epochs=2500, step=100)
+
+    # Check the costs
+    assert pytest.approx(model.costs[0]) == 0.7717493284237686
+    assert pytest.approx(model.costs[1]) == 0.6720534400822913
+    assert pytest.approx(model.costs[2]) == 0.6482632048575212
+
+    assert pytest.approx(model.costs[-3]) == 0.10285466069352679
+    assert pytest.approx(model.costs[-2]) == 0.10089745445261786
+    assert pytest.approx(model.costs[-1]) == 0.09287821526472397
+
+    # Check accuracy score
+    assert pytest.approx(model.accuracy_score(train_set_x, train_set_y)) == 0.9856459330143541
+    assert model.accuracy_score(test_set_x, test_set_y) == 0.8
+
+
+def test_deep_nn_2():
+    train_x, train_y, test_x, test_y = load_2d_dataset()
+
+    model = NeuralNetwork([20, 3, 1])
+    weight = initialize_parameters([train_x.shape[1], 20, 3, 1], 3)
+    model.set_params(train_x.shape[1], weight=weight)
+
+    model.fit(train_x, train_y, learning_rate=0.3, epochs=30_000, step=10_000)
+
+    # plt.rcParams['figure.figsize'] = (7.0, 4.0)
+    # axes = plt.gca()
+    # axes.set_xlim([-0.75, 0.40])
+    # axes.set_ylim([-0.75, 0.65])
+
+    # plt.title("Model without regularization")
+    # plot_decision_boundary(model, train_x, train_y)
+
+    # Check the costs
+    assert pytest.approx(model.costs) == [0.6557412523481002, 0.1632998752572421, 0.13851642423244123]
+
+    # Check accuracy score
+    assert pytest.approx(model.accuracy_score(train_x, train_y)) == 0.9478672985781991
+    assert model.accuracy_score(test_x, test_y) == 0.915
+
+
+def test_L2_regularization():
+    train_x, train_y, test_x, test_y = load_2d_dataset()
+
+    model = NeuralNetwork([20, 3, 1])
+    weight = initialize_parameters([train_x.shape[1], 20, 3, 1], 3)
+    model.set_params(train_x.shape[1], weight=weight)
+
+    model.fit(train_x, train_y, learning_rate=0.3, lambd=0.7, epochs=30_000, step=10_000)
+
+    # plt.rcParams['figure.figsize'] = (7.0, 4.0)
+    # axes = plt.gca()
+    # axes.set_xlim([-0.75, 0.40])
+    # axes.set_ylim([-0.75, 0.65])
+
+    # plt.title("Model with L2 regularization")
+    # plot_decision_boundary(model, train_x, train_y)
+
+    # Check the costs
+    assert pytest.approx(model.costs) == [0.6974484493131264, 0.2684918873282238, 0.2680916337127302]
+
+    # Check accuracy score
+    assert model.accuracy_score(train_x, train_y) == 0.9383886255924171
+    assert model.accuracy_score(test_x, test_y) == 0.93
+
+
+def test_forward_pass_dropout():
+    np.random.seed(1)
+    X_assess = np.random.randn(3, 5).T
+
+    W1 = np.random.randn(2, 3).T
+    b1 = np.random.randn(2, 1).T
+
+    W2 = np.random.randn(3, 2).T
+    b2 = np.random.randn(3, 1).T
+
+    W3 = np.random.randn(1, 3).T
+    b3 = np.random.randn(1, 1)
+
+    model = NeuralNetwork([2, 3, 1], seed=1)
+    model.set_params(X_assess.shape[1], weight={"W1": W1, "W2": W2, "W3": W3}, bias={"b1": b1, "b2": b2, "b3": b3})
+    model.set_activations()
+
+    # Check forward propagation
+    y, _ = model.forward_pass(X_assess, [.7]*2)
+    assert np.allclose(y, [[0.36974721], [0.49683389], [0.04565099], [0.01446893], [0.36974721]])
+
+
+def test_dropout():
+    train_x, train_y, test_x, test_y = load_2d_dataset()
+
+    model = NeuralNetwork([20, 3, 1], seed=24)
+    weight = initialize_parameters([train_x.shape[1], 20, 3, 1], 3)
+    model.set_params(train_x.shape[1], weight=weight)
+
+    model.fit(train_x, train_y, learning_rate=0.3, keep_prob=0.8, epochs=30_000, step=10_000)
+
+    # plt.rcParams['figure.figsize'] = (7.0, 4.0)
+    # axes = plt.gca()
+    # axes.set_xlim([-0.75, 0.40])
+    # axes.set_ylim([-0.75, 0.65])
+    #
+    # plt.title("Model with dropout")
+    # plot_decision_boundary(model, train_x, train_y)
+
+    # Check the costs
+    assert pytest.approx(model.costs) == [0.6585998432035588, 0.20187690743859132, 0.15421839870569154]
+
+    # Check accuracy score
+    assert pytest.approx(model.accuracy_score(train_x, train_y)) == 0.957345971563981
+    assert model.accuracy_score(test_x, test_y) == 0.94
+
+
+def test_zero_params():
+    train_x, train_y = make_circles(n_samples=300, noise=.05, random_state=1)
+    test_X, test_Y = make_circles(n_samples=100, noise=.05, random_state=2)
+
+    model = NeuralNetwork([20, 3, 1])
+    weights = {}
+    layer_dims = [train_x.shape[1], 20, 3, 1]
+
+    for l in range(1, 4):
+        weights['W' + str(l)] = np.zeros((layer_dims[l - 1], layer_dims[l]))
+    model.set_params(train_x.shape[1], weight=weights)
+
+    model.fit(train_x, train_y, learning_rate=0.01, epochs=15_000, step=1_000)
+
+    assert all(pytest.approx(x) == 0.6931471805599453 for x in model.costs)
+    assert model.accuracy_score(train_x, train_y) == 0.5
+    assert model.accuracy_score(test_X, test_Y) == 0.5
+
+
+def test_big_weights():
+    train_x, train_y = make_circles(n_samples=300, noise=.05, random_state=1)
+    test_X, test_Y = make_circles(n_samples=100, noise=.05, random_state=2)
+
+    model = NeuralNetwork([10, 5, 1])
+    weights = {}
+    layer_dims = [train_x.shape[1], 10, 5, 1]
+
+    np.random.seed(3)
+    for l in range(1, 4):
+        weights['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1]).T * 10
+    model.set_params(train_x.shape[1], weight=weights)
+
+    model.fit(train_x, train_y, learning_rate=0.01, epochs=15_000, step=1_000)
+
+    # Check the costs
+    assert model.costs[0] == float("inf")
+
+    # Check accuracy score
+    assert model.accuracy_score(train_x, train_y) == 0.5 == model.accuracy_score(test_X, test_Y)
+
+
+def test_he_init():
+    train_x, train_y = make_circles(n_samples=300, noise=.05, random_state=1)
+    test_X, test_Y = make_circles(n_samples=100, noise=.05, random_state=2)
+
+    model = NeuralNetwork([10, 5, 1], seed=24)
+    model.fit(train_x, train_y, learning_rate=0.01, epochs=15_000, step=1_000)
+
+    # Check the costs
+    assert pytest.approx(model.costs[0]) == 0.712495051645707
+    assert pytest.approx(model.costs[1]) == 0.6732084171360789
+    assert pytest.approx(model.costs[2]) == 0.6491058169126
+
+    assert pytest.approx(model.costs[-3]) == 0.08220191573225748
+    assert pytest.approx(model.costs[-2]) == 0.07588034042714038
+    assert pytest.approx(model.costs[-1]) == 0.07088313671881122
+
+    # Check accuracy score
+    assert model.accuracy_score(train_x, train_y) == 0.98
+    assert model.accuracy_score(test_X, test_Y) == 0.96
+
+
+def test_gradient_check_1():
+    train_x, train_y, _, _ = load_2d_dataset()
+    model = NeuralNetwork([5, 3, 1], seed=10)
+
+    # Without L2 regularization
+    model.check_gradient(train_x, train_y)
+    assert model.costs == []
+    assert len(model.parameters) == 0
+    assert len(model.activations) == 0
+
+    # With L2 regularization
+    model.check_gradient(train_x, train_y, lambd=0.02)
+    assert model.costs == []
+    assert len(model.parameters) == 0
+    assert len(model.activations) == 0
+
+
+def test_gradient_check_2():
+    w = {}
+    b = {}
+    np.random.seed(1)
+
+    x = np.random.randn(100, 4)
+    y = np.random.randint(2, size=100)
+
+    w['W1'] = np.random.randn(4, 5)
+    w['W2'] = np.random.randn(5, 3)
+    w['W3'] = np.random.randn(3, 1)
+    b['b2'] = np.random.randn(1, 3)
+    b['b1'] = np.random.randn(1, 5)
+    b['b3'] = np.random.randn(1, 1)
+
+    backup_w = {key: value.copy() for key, value in w.items()}
+    backup_b = {key: value.copy() for key, value in b.items()}
+
+    model = NeuralNetwork([5, 3, 1])
+    model.set_params(n_att=4, weight=w, bias=b)
+
+    # Without L2 regularization
+    model.check_gradient(x, y)
+    assert model.costs == []
+    assert len(model.activations) == 0
+    assert all(np.all(value == model.parameters[key]) for key, value in backup_w.items())
+    assert all(np.all(value == model.parameters[key]) for key, value in backup_b.items())
+
+    # With L2 regularization
+    model.check_gradient(x, y, lambd=0.05)
+    assert model.costs == []
+    assert len(model.activations) == 0
+    assert all(np.all(value == model.parameters[key]) for key, value in backup_w.items())
+    assert all(np.all(value == model.parameters[key]) for key, value in backup_b.items())
+
+
+def test_gradient_check_3():
+    x = np.random.randn(100, 4)
+    y = np.random.randint(2, size=100)
+    model = NeuralNetwork([5, 3, 1], seed=24)
+
+    # Wrong derivative
+    def false_relu(z, derivative=False):
+        if derivative:
+            return np.where(z < 0, 0.01, 1.)
+        return np.where(z < 0, 0., z)
+
+    activations = [tanh, false_relu, sigmoid]
+    model.set_activations(activations)
+
+    with pytest.raises(AssertionError):
+        model.check_gradient(x, y)
+    assert model.costs == []
+    assert len(model.parameters) == 0
+    assert len(model.activations) == 3
+    assert model.activations == activations
+
+    # Correct derivative
+    activations = [tanh, relu, sigmoid]
+    model.set_activations(activations)
+    model.check_gradient(x, y)
+
+    assert model.costs == []
+    assert len(model.parameters) == 0
+    assert len(model.activations) == 3
+    assert model.activations == activations
+
+
+def test_mini_batch():
+    model = NeuralNetwork([5, 2, 1], seed=3)
+    model.fit(moons_X, moons_y, learning_rate=0.0007, epochs=5000, step=1000, batch_size=64, shuffle=True)
+
+    assert pytest.approx(model.costs) == [0.7588353088246047, 0.5977185220351452, 0.5316795298099085,
+                                          0.49401085257243593, 0.4635011585260269]
+    assert pytest.approx(model.accuracy_score(moons_X, moons_y)) == 0.8066666666666666
+
+
+def test_momentum_optimizer():
+    model = NeuralNetwork([5, 2, 1], seed=3)
+    model.fit(moons_X, moons_y,
+              learning_rate=.0007,
+              epochs=5000, step=1000,
+              batch_size=64, shuffle=True,
+              optimizer='momentum')
+
+    assert pytest.approx(model.costs) == [0.7590075570464446, 0.5979284827243367, 0.5318105436146895,
+                                          0.4940913450754209, 0.4635575132244337]
+    assert pytest.approx(model.accuracy_score(moons_X, moons_y)) == 0.8066666666666666
+
+
+def test_rmsprop_optimizer():
+    model = NeuralNetwork([5, 2, 1], seed=3)
+    model.fit(moons_X, moons_y,
+              learning_rate=.0007,
+              epochs=5000, step=1000,
+              batch_size=64, shuffle=True,
+              optimizer='rmsprop')
+
+    assert pytest.approx(model.costs) == [0.7366576114998906, 0.1817141962462617, 0.1427976235658836,
+                                          0.1410125678891975, 0.1421516720403639]
+    assert pytest.approx(model.accuracy_score(moons_X, moons_y)) == 0.9433333333333334
+
+
+def test_adam_optimizer():
+    model = NeuralNetwork([5, 2, 1], seed=3)
+    model.fit(moons_X, moons_y,
+              learning_rate=.0007,
+              epochs=5000, step=1000,
+              batch_size=64, shuffle=True,
+              optimizer='adam')
+
+    assert pytest.approx(model.costs) == [0.7579587937629741, 0.2540861504539721, 0.1459760758188075,
+                                          0.1423536136409726, 0.1420671741058317]
+    assert pytest.approx(model.accuracy_score(moons_X, moons_y)) == 0.9433333333333334
